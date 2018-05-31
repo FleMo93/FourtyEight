98ffd9cd9bdc0c6416090f52ececfaf7f6f86c2a
Florian Hammer <florianhammer@hotmail.de>
2018-05-31 21:25:42 +0200

    Reset

---
+++ Assets/FourtyEight/Code/Buildings/Extractor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Extractor : MonoBehaviour
+{
+    [SerializeField]
+    private so_DataSet _Stats;
+    [SerializeField]
+    private so_DataSetGlobal _StatsGlobal;
+
+    private so_DataSet.Attribute IronToGain;
+    private so_DataSet.Attribute TickUntilGain;
+
+
+    private float timeLeft = 0;
+
+    void Start ()
+    {
+        IronToGain = _Stats.Attributes.Find(x => x.Name == "Iron to gain");
+        TickUntilGain = _Stats.Attributes.Find(x => x.Name == "Tick until gain");
+
+        timeLeft = TickUntilGain.Value;
+    }
+	
+	void Update ()
+    {
+        if(_StatsGlobal.Energy < 0)
+        {
+            return;
+        }
+
+        timeLeft -= Time.deltaTime;
+
+        if(timeLeft <= 0)
+        {
+            _StatsGlobal.Iron += (int)IronToGain.Value;
+        }
+    }
+}
