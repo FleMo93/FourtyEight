bd64389ba125ff4e5fd4e4dac24fd580b8e34b05
Florian Hammer <florianhammer@hotmail.de>
2018-06-01 00:55:24 +0200

    Rename cristals

--- Assets/FourtyEight/Code/Buildings/Generator.cs
+++ Assets/FourtyEight/Code/Buildings/Generator.cs
@@ -11,19 +11,50 @@ public class Generator : MonoBehaviour
 
     private scr_DataSet.Attribute health;
     private so_DataSet.Attribute healthMax;
+    private so_DataSet.Attribute createsEnergy;
     private float timeLeft = 0;
 
     private void Start()
     {
         healthMax = _Stats.Attributes.Find(x => x.Name == "Maximum Health");
         health = GetComponent<scr_DataSet>().Attributes.Find(x => x.Name == "Health");
-
+        createsEnergy = _Stats.Attributes.Find(x => x.Name == "Creates energy");
         health.Value = healthMax.Value;
         timeLeft = 1; 
     }
 
+    bool lastEnergyState = false;
     void Update()
     {
-        
+        if(health.Value <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+
+        timeLeft -= Time.deltaTime;
+
+        if (timeLeft <= 0)
+        {
+            if (_StatsGlobal.Coal > 0)
+            {
+                _StatsGlobal.Coal -= 1;
+
+                if (!lastEnergyState)
+                {
+                    _StatsGlobal.Energy += (int)createsEnergy.Value;
+                    lastEnergyState = true;
+                }
+            }
+            else
+            {
+                if (lastEnergyState)
+                {
+                    _StatsGlobal.Energy -= (int)createsEnergy.Value;
+                    lastEnergyState = false;
+                }
+            }
+
+            timeLeft += 1;
+        }
     }
 }
