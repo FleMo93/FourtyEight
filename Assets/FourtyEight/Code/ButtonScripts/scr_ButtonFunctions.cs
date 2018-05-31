88167d4bd95353e42fe9e9f09934b71db870a6f3
Icrodiscfe <33602499+Icrodiscfe@users.noreply.github.com>
2018-05-31 14:50:50 +0200

    Tomek-20180531-1450

---
+++ Assets/FourtyEight/Code/ButtonScripts/scr_ButtonFunctions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scr_ButtonFunctions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void BtnClick_LoadScene(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName, LoadSceneMode.Single);
+    }
+
+    public void BtnClick_QuitGame()
+    {
+        Application.Quit();
+    }
+}
