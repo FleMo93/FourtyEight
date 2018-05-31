dfb024be6f928b87d9167530c67788cedd620e5c
Icrodiscfe <33602499+Icrodiscfe@users.noreply.github.com>
2018-05-31 20:14:06 +0200

    Tomek-20180531-2014

---
+++ Assets/FourtyEight/Code/UserInterface/scr_Infobox.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scr_Infobox : MonoBehaviour {
+
+    public so_DataSet DataSet;
+    public Image Icon;
+    public Text Name;
+    public Text Description;
+    public List<UiAttribute> Attributes;
+
+	// Use this for initialization
+	void Start () {
+        Icon.sprite = DataSet.MainIcon;
+        Name.text = DataSet.Name;
+        Description.text = DataSet.Desctription;
+
+        for (int i = 0; i < Attributes.Count; i++)
+        {
+            if ( i < DataSet.Attributes.Count)
+            {
+                Attributes[i].Icon.sprite = DataSet.Attributes[i].Icon;
+                Attributes[i].Value.text = DataSet.Attributes[i].Value.ToString();
+            }
+            else
+            {
+                Attributes[i].Object.SetActive(false);
+            }
+
+        }
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    [System.Serializable]
+    public class UiAttribute
+    {
+        public GameObject Object;
+        public Image Icon;
+        public Text Value;
+    }
+}
