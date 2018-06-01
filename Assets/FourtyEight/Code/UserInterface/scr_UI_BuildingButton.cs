a6d8d0968108801905edc291eb5694f5368e49f5
Icrodiscfe <33602499+Icrodiscfe@users.noreply.github.com>
2018-06-01 02:46:21 +0200

    Tomek

---
+++ Assets/FourtyEight/Code/UserInterface/scr_UI_BuildingButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Linq;
+
+public class scr_UI_BuildingButton : MonoBehaviour, IPointerDownHandler {
+
+    public so_DataSet DataSet;
+    public so_DataSetGlobal GlobalDataSet;
+
+    List<so_DataSet.Attribute> attToShow;
+
+
+    // Use this for initialization
+    void Start () {
+        //attToShow = DataSet.Attributes.ToArray().Where(x => x.Name.ToLower().Contains("cost")).ToList();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public void OnPointerDown(PointerEventData data)
+    {
+        if (this.GetComponent<Button>().interactable)
+        {
+
+        }
+    }
+}
