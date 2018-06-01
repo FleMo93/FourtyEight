d8921d4b622702ebd1058544b2e189a64721be02
Icrodiscfe <33602499+Icrodiscfe@users.noreply.github.com>
2018-06-01 03:30:48 +0200

    Tomek

--- Assets/FourtyEight/Code/UserInterface/scr_UI_BuildingButton.cs
+++ Assets/FourtyEight/Code/UserInterface/scr_UI_BuildingButton.cs
@@ -10,18 +10,41 @@ public class scr_UI_BuildingButton : MonoBehaviour, IPointerDownHandler {
     public so_DataSet DataSet;
     public so_DataSetGlobal GlobalDataSet;
 
-    List<so_DataSet.Attribute> attToShow;
+    public Color NormalColor;
+    public Color InactiveColor;
+
+    float CostStone;
+    float CostCoal;
+    float CostIron;
+    float CostDida;
+    float CostGale;
+    float CostEnergy;
 
 
     // Use this for initialization
     void Start () {
-      //  attToShow = DataSet.Attributes.ToArray().Where(x => x.Name == scr_Attributes.Attribute.cos
+        try
+        {
+            CostStone = DataSet.Attributes.Find(x => x.Name == scr_Attributes.Attribute.Cost_Stone).Value;
+            CostCoal = DataSet.Attributes.Find(x => x.Name == scr_Attributes.Attribute.Cost_Coal).Value;
+            CostIron = DataSet.Attributes.Find(x => x.Name == scr_Attributes.Attribute.Cost_Iron).Value;
+            CostDida = DataSet.Attributes.Find(x => x.Name == scr_Attributes.Attribute.Cost_Dida).Value;
+            CostGale = DataSet.Attributes.Find(x => x.Name == scr_Attributes.Attribute.Cost_Gale).Value;
+            CostEnergy = DataSet.Attributes.Find(x => x.Name == scr_Attributes.Attribute.Cost_Energy).Value;
+        }
+        catch (System.Exception)
+        {
+
+            throw;
+        }
     }
 
     // Update is called once per frame
     void Update () {
-		
-	}
+        bool buildable = GlobalDataSet.Coal >= CostCoal && GlobalDataSet.Stone >= CostStone && GlobalDataSet.Iron >= CostIron && GlobalDataSet.CrystalDida >= CostDida &&
+            GlobalDataSet.CrystalGale >= CostGale && GlobalDataSet.Energy >= CostEnergy;
+
+    }
 
     public void OnPointerDown(PointerEventData data)
     {
