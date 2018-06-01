96703fb6e87060894aa283d2a0469e3b9ad59723
Florian Hammer <florianhammer@hotmail.de>
2018-06-01 18:19:08 +0200

    Revert "Revert "Merge branch 'master' of https://github.com/FleMo93/FourtyEight""
    This reverts commit d294f5a13ccc93da2749e8490c22d96da9721f55.

---
+++ Assets/FourtyEight/Code/UserInterface/scr_ButtonFunctions_Back.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scr_ButtonFunctions_Back : MonoBehaviour {
+    
+    public void BtnClick_LoadScene(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName, LoadSceneMode.Single);
+    }
+}
