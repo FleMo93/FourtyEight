bd9bc9c8e4960f58afd3c87a22b94214411249d5
Florian Hammer <florianhammer@hotmail.de>
2018-06-01 19:35:42 +0200

    WaveSpawner startfwaerg aertdhgb gxdf

---
+++ Assets/FourtyEight/Code/Level/scr_WaveSet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New WaveSet", menuName = "ScriptabeObjects/New wave set")]
+public class scr_WaveSet : ScriptableObject
+{
+    public scr_Wave[] Waves;
+
+
+    [System.Serializable]
+    public class scr_Wave
+    {
+        public float SpawnAfterSeconds;
+        public scr_Group[] Groups;
+    }
+
+    [System.Serializable]
+    public class scr_Group
+    {
+        public GameObject EnemyToSpawnPrefab;
+        public int Count = 1;
+    }
+}
