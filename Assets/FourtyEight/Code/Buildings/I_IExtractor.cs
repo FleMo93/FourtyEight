bc663f61409b2577859a9cb2149a2726ae360ccc
Florian Hammer <florianhammer@hotmail.de>
2018-06-01 15:54:27 +0200

    Fixed extractor 2

---
+++ Assets/FourtyEight/Code/Buildings/I_IExtractor.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface I_IExtractor
+{
+    void SetRessourceToTake(scr_Attributes.Attribute ressourceToTake);
+}
